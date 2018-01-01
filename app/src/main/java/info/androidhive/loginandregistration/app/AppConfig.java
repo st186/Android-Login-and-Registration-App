e08e51cd3b54f632fd416c74ff38ab66e99d3d07
Subham Tewari <subham.tiwari186@gmail.com>
2018-01-01 11:27:22 +0530

    Android App for login and Registration
    A great android app for login and registration with database support

---
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.androidhive.loginandregistration.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://172.16.45.55/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://172.16.45.55/android_login_api/register.php";
+}
